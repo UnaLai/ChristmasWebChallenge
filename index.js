6e267374b6e88514df25c839c9925d92667390cb
JasonYang <jason19901207@gmail.com>
2018-12-25 13:42:35 +0800

    voice

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ window.onload = function() {
             let music = [{ a: "C", b: 4, c: 2000 }, { a: "D", b: 4, c: 2000 }, { a: "E", b: 4, c: 2000 }];
             music.forEach(item => {
                 console.log(time);
-                piano.play('C', 4, 2)
-                setTimeout(piano.play(item.a, item.b, item.c / 1000), time);
+                // piano.play('C', 4, 2)
+                setTimeout(() => { piano.play(item.a, item.b, item.c / 1000) }, time);
                 // piano.play(item.a, item.b, item.c / 1000);
                 time += item.c;
             });
