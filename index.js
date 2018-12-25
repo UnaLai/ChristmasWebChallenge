8fb0d5acd6ca7ea7c696b6dd28453a8981327ade
JasonYang <jason19901207@gmail.com>
2018-12-25 13:31:17 +0800

    voice

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 Synth instanceof AudioSynth; // true
 
-var testInstance = new AudioSynth;
-testInstance instanceof AudioSynth; // true
+// var testInstance = new AudioSynth;
+// testInstance instanceof AudioSynth; // true
 
-testInstance === Synth; // true
+// testInstance === Synth; // true
 
 var piano = Synth.createInstrument('piano');
 
 window.onload = function() {
-  var context = new AudioContext();
-  // Setup all nodes
-  document.querySelector('button').addEventListener('click', function() {
-    context.resume().then(() => {
-      piano.play('C', 4, 2);
+    var context = new AudioContext();
+    // Setup all nodes
+    document.querySelector('button').addEventListener('click', function() {
+        context.resume().then(() => {
+        function play() {
+            var time = 0;
+            let music = [{ a: "C", b: 4, c: 2000 }, { a: "D", b: 4, c: 2000 }, { a: "E", b: 4, c: 2000 }];
+            music.forEach(item => {
+                console.log(time);
+                piano.play('C', 4, 2)
+                setTimeout(piano.play(item.a, item.b, item.c / 1000), time);
+                // piano.play(item.a, item.b, item.c / 1000);
+                time += item.c;
+            });
+        }
+        play();
     });
-  });
+    });
 }
+
