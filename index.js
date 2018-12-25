21a37fddeb31cdf69ec88584530dd80316ba819d
UnaLai <qqq323618@gmail.com>
2018-12-25 14:24:24 +0800

    music play

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Synth instanceof AudioSynth; // true
 // testInstance === Synth; // true
 
 var piano = Synth.createInstrument('piano');
+var speed = 400;
 
 window.onload = function() {
     var context = new AudioContext();
@@ -13,18 +14,39 @@ window.onload = function() {
     document.querySelector('button').addEventListener('click', function() {
         context.resume().then(() => {
         function play() {
-            var time = 0;
-            let music = [{ a: "C", b: 4, c: 2000 }, { a: "D", b: 4, c: 2000 }, { a: "E", b: 4, c: 2000 }];
+            var musictime = 0;
+            let music = [{ key: "E", interval: 4, playtime: 2*speed },{ key: "G", interval: 4, playtime: 2*speed },
+            { key: "E", interval: 4, playtime: 1*speed },{ key: "G", interval: 4, playtime: 1*speed },
+            { key: "D", interval: 5, playtime: 2*speed },{ key: "E", interval: 5, playtime: 2*speed },
+            { key: "E", interval: 4, playtime: 2*speed },{ key: "G", interval: 4, playtime: 2*speed },
+            { key: "E", interval: 4, playtime: 1*speed },{ key: "G", interval: 4, playtime: 1*speed },
+            { key: "D", interval: 5, playtime: 2*speed },{ key: "E", interval: 5, playtime: 2*speed }];
+
             music.forEach(item => {
-                console.log(time);
                 // piano.play('C', 4, 2)
-                setTimeout(() => { piano.play(item.a, item.b, item.c / 1000) }, time);
+                setTimeout(() => { piano.play(item.key, item.interval, item.playtime / 1000) }, musictime);
                 // piano.play(item.a, item.b, item.c / 1000);
-                time += item.c;
+                musictime += item.playtime;
             });
+
+            var chordtime = 0;
+            let chord=[
+            [{ key: "C", interval: 4, playtime: 6*speed },{ key: "E", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}],
+            [{ key: "C", interval: 4, playtime: 6*speed },{ key: "E", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}],
+            [{ key: "B", interval: 4, playtime: 6*speed },{ key: "D", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}],
+            [{ key: "B", interval: 4, playtime: 6*speed },{ key: "D", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}]
+            ];
+            chord.forEach(item => {
+              setTimeout(() => {
+                item.forEach(single => {
+                  piano.play(single.key, single.interval, single.playtime / 1000);
+                })
+              },chordtime);
+                chordtime += 6*speed;
+            })
+
         }
         play();
     });
     });
 }
-
