373672f97df7055ee4645dde46f86a982ac6e83e
UnaLai <qqq323618@gmail.com>
2018-12-26 17:44:43 +0800

    modify coding style

--- index.js
+++ index.js
@@ -1,54 +1,181 @@
-Synth instanceof AudioSynth;
+(function() {
+  //建立鋼琴實例
+  Synth instanceof AudioSynth;
+  var piano = Synth.createInstrument('piano');
 
+  //音樂速度設定
+  var speed = 300;
 
-var piano = Synth.createInstrument('piano');
+  //樂譜
+  let rightHandSpectrum = [{
+      key: "E",
+      interval: 4,
+      playtime: 2 * speed
+    },
+    {
+      key: "G",
+      interval: 4,
+      playtime: 2 * speed
+    },
+    {
+      key: "E",
+      interval: 4,
+      playtime: 1 * speed
+    },
+    {
+      key: "G",
+      interval: 4,
+      playtime: 1 * speed
+    },
+    {
+      key: "D",
+      interval: 5,
+      playtime: 2 * speed
+    },
+    {
+      key: "E",
+      interval: 5,
+      playtime: 2 * speed
+    },
+    {
+      key: "E",
+      interval: 4,
+      playtime: 2 * speed
+    },
+    {
+      key: "G",
+      interval: 4,
+      playtime: 2 * speed
+    },
+    {
+      key: "E",
+      interval: 4,
+      playtime: 1 * speed
+    },
+    {
+      key: "G",
+      interval: 4,
+      playtime: 1 * speed
+    },
+    {
+      key: "D",
+      interval: 5,
+      playtime: 2 * speed
+    },
+    {
+      key: "E",
+      interval: 5,
+      playtime: 2 * speed
+    }
+  ];
 
-//音樂設定
-var speed = 380;
-var musictime = 0;
-var chordtime = 0;
+  let leftHandSpectrum = [
+    [{
+        key: "C",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "E",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "G",
+        interval: 4,
+        playtime: 2 * speed
+      }
+    ],
+    [{
+        key: "C",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "E",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "G",
+        interval: 4,
+        playtime: 2 * speed
+      }
+    ],
+    [{
+        key: "B",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "D",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "G",
+        interval: 4,
+        playtime: 2 * speed
+      }
+    ],
+    [{
+        key: "B",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "D",
+        interval: 4,
+        playtime: 2 * speed
+      },
+      {
+        key: "G",
+        interval: 4,
+        playtime: 2 * speed
+      }
+    ]
+  ];
 
-//樂譜
-let music = [{ key: "E", interval: 4, playtime: 2*speed },{ key: "G", interval: 4, playtime: 2*speed },
-{ key: "E", interval: 4, playtime: 1*speed },{ key: "G", interval: 4, playtime: 1*speed },
-{ key: "D", interval: 5, playtime: 2*speed },{ key: "E", interval: 5, playtime: 2*speed },
-{ key: "E", interval: 4, playtime: 2*speed },{ key: "G", interval: 4, playtime: 2*speed },
-{ key: "E", interval: 4, playtime: 1*speed },{ key: "G", interval: 4, playtime: 1*speed },
-{ key: "D", interval: 5, playtime: 2*speed },{ key: "E", interval: 5, playtime: 2*speed }];
+  let container = document.querySelector('.container');
 
-let chord=[
-[{ key: "C", interval: 4, playtime: 6*speed },{ key: "E", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}],
-[{ key: "C", interval: 4, playtime: 6*speed },{ key: "E", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}],
-[{ key: "B", interval: 4, playtime: 6*speed },{ key: "D", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}],
-[{ key: "B", interval: 4, playtime: 6*speed },{ key: "D", interval: 4, playtime: 6*speed},{ key: "G", interval: 4, playtime: 6*speed}]
-];
-
-//畫面隨節奏變化
-let container = document.querySelector('.container');
-
-window.onload = function() {
-    var context = new AudioContext();
-    document.querySelector('button').addEventListener('click', function() {
-        context.resume().then(() => {
-        function play(music,chord) {
-            music.forEach(item => {
-                setTimeout(() => {
-                  piano.play(item.key, item.interval, item.playtime / 1000);
-                  container.classList.add("beat");
-                  setTimeout(()=>{ container.classList.remove("beat")},300); }
-                  , musictime);
-                musictime += item.playtime;
-            });
-            chord.forEach(item => {
-              setTimeout(() => {
-                item.forEach(single => {
-                  piano.play(single.key, single.interval, single.playtime / 1000);
-                });
-              },chordtime);
-                chordtime += 6*speed;
-            });
-        }
-        play(music,chord);
+  window.onload = function() {
+    document.querySelector('button').addEventListener('click', () => {
+      play(rightHandSpectrum, leftHandSpectrum)
     });
-    });
-}
+
+    function play(rightHandSpectrum, leftHandSpectrum) {
+      rightHandPlay(rightHandSpectrum);
+      leftHandPlay(leftHandSpectrum);
+    }
+
+    function rightHandPlay(spectrum) {
+      let rightHandTime = 0;
+      spectrum.forEach(item => {
+        setTimeout(() => {
+          piano.play(item.key, item.interval, item.playtime / 1000);
+          musicEffect();
+        }, rightHandTime);
+        rightHandTime += item.playtime;
+      });
+    }
+
+    function leftHandPlay(spectrum) {
+      let leftHandTime = 0;
+      spectrum.forEach(item => {
+        setTimeout(() => {
+          item.forEach(single => {
+            piano.play(single.key, single.interval, single.playtime / 1000);
+          });
+        }, leftHandTime);
+        leftHandTime += 6 * speed;
+      });
+    }
+
+    function musicEffect() {
+      container.classList.add("beat");
+      setTimeout(() => {
+        container.classList.remove("beat")
+      }, 300);
+    }
+  }
+})();
